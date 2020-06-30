093e8116de1063a5b786c6f14d23e4fa131c18fe
Eric huang <1659897298@qq.com>
2020-06-30 21:49:36 +0800

    move choose page

--- lib/page/choose_application_page/action.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fish_redux/fish_redux.dart';
-
-enum ChooseApplicationAction { changeCamera }
-
-class ChooseApplicationActionCreator {
-  static Action onChangeCamera(int selectIndex) {
-    return Action(ChooseApplicationAction.changeCamera,payload: selectIndex);
-  }
-}
